3f23747aa12ae22900f2fed6177239f7bbf3217b
Александр Смирнов <alexrus37reg@gmail.com>
2023-08-15 14:19:37 +0300

    fix: animation: one click-scroll-one animation

--- utils/animations/headerAnimation.js
+++ utils/animations/headerAnimation.js
@@ -1,12 +1,17 @@
 export function animateHeader(gsap) {
+  let posTop;
   const tlHeader = gsap.timeline({
-    // scrollTrigger: {
-    //   trigger: ".section__header-wrapper",
-    //   start: "top top",
-    //   end: "+=5000",
-    //   scrub: 1.5,
-    //   pin: true,
-    // },
+    scrollTrigger: {
+      trigger: ".section__header-wrapper",
+      start: "top+=1px top",
+      end: "+=800",
+      //scrub: 1,
+      toggleActions: "play none none reverse",
+      pin: true,
+      onUpdate: (self) => {
+        if (posTop < self.scroller.pageYOffset) tlHeader.play();
+      },
+    },
   });
   tlHeader.set(".section__header-subtitle", { opacity: 0, yPercent: 100 });
   tlHeader.set(".section__header-title", { color: "#000" });
@@ -15,10 +20,15 @@ export function animateHeader(gsap) {
     xPercent: -10,
     duration: 3,
   });
+
   tlHeader.to(
     ".section__header-title",
-    { color: "#fff", duration: 1.5, },
-    "-=1.3"
+    {
+      color: "#fff", duration: 1.5, onComplete: () => {
+        posTop = window.scrollY;
+        tlHeader.pause();
+      }
+    }, "-=1.5"
   );
   tlHeader.to(
     ".section__header-img",
@@ -27,18 +37,24 @@ export function animateHeader(gsap) {
   );
   tlHeader.to(
     ".section__header-title",
-    { top: "41%", yPercent: -41, duration: 1 },
+    {
+      top: "41%", yPercent: -41, duration: 1
+    },
     "-=1"
   );
   tlHeader.to(
     ".section__header-subtitle",
-    { opacity: 1, yPercent: 0 },
+    {
+      opacity: 1, yPercent: 0, onComplete: () => {
+        posTop = window.scrollY;
+        tlHeader.pause();
+      }
+    },
     "-=0.5"
   );
   tlHeader.to(
     ".section__header",
-    { scale: "10", duration: 1 },
-    "+=0.5"
+    { scale: "10", duration: 3 },
   );
   tlHeader.to(".section__header-title", { opacity: 0, duration: 0.1 }, "-=0.7");
 }
